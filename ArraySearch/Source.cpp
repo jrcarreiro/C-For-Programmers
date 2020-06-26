4d07e27c08196dd896ea25020f728cf22daeea3f
Junior Carreiro <junior.carreiro@outlook.com>
2020-06-26 08:53:53 -0300

    Array searches

---
+++ ArraySearch/Source.cpp
@@ -0,0 +1,48 @@
+/*Goal: practice searching an array in C++
+**There is an array of integers. The length of the arrays to be searched
+**varies. A user will enter a postive integer and the program will search the array.
+**If the value is in the array, the program will return the location of
+**the element. If the value is not in the array, the user will be notified
+**that the value is not in the array. To exit the program the user will enter -1.
+*/
+
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    int searchKey = 0;
+    int searchArray[10] = { 324,4567,6789,5421345,7,65,8965,12,342,485 };
+    int location = 0;
+
+    while (1)
+    {
+	   cout << "Enter an integer ('-1' to quit): ";
+	   scanf_s("%d", &searchKey);
+	   cout << searchKey << endl;
+	   if (searchKey == -1)
+	   {
+		  break;
+	   }
+	   for (size_t i = 0; i < 10; i++)
+	   {
+		  if (searchKey == searchArray[i])
+		  {
+			 location = i;
+			 break;
+		  }
+		  location = -1;
+	   }
+	   if (location != -1)
+	   {
+		  cout << searchKey << " is at location " << location << " in the array." << endl;
+	   }
+	   else
+	   {
+		  cout << searchKey << " is not in the array." << endl;
+	   }
+	   return 0;
+    }
+}
