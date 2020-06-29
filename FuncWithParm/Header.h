9c169bb4199868ba42ae2e834bcf8e6ea1af20fe
Junior Carreiro <junior.carreiro@outlook.com>
2020-06-29 07:39:38 -0300

    Functions and headers files

---
+++ FuncWithParm/Header.h
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+void printProduct(int m1, int m2, int product)
+{
+    cout << m1 << " * " << m2 << " = " << product << endl;
+}
---
+++ FuncWithParm/Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "Header.h"
+
+using namespace std;
+
+void printProduct(int m1, int m2, int product);
+
+int main()
+{
+    int m1 = 4;
+    int m2 = 5;
+    int product;
+
+    product = m1 * m2;
+
+    printProduct(m1, m2, product);
+    return 0;
+}
